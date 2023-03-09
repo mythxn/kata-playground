cfd88f48b120c8021b4214ccdd74ca69fdb88b7e
Mithun <mythxn@pm.me>
2023-03-09 15:03:04 +0400

    Baby Calculator

--- app/main.py
+++ app/main.py
@@ -1,2 +1,15 @@
 def hello_world():
     return 'Hello World!'
+
+
+def baby_multi(a: int, b: int) -> int:
+    baby_sum = 0
+    try:
+        for _ in range(abs(b)):
+            baby_sum += abs(a)
+
+        negative = (a < 0 or b < 0) and (a >= 0 or b >= 0)
+        return 0 - baby_sum if negative else baby_sum
+
+    except TypeError:
+        return None
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,21 @@ from app.main import *
 
 def test_hello_world():
     assert hello_world() == 'Hello World!'
+
+
+# The task in this section is to build a baby calculator in TDD style. We are
+# building our own calculator with a minimal set of features. Write a function
+# that takes two parameters and returns the multiplication without using the
+# multiplication.
+
+
+def test_baby_calc_multipllication():
+    assert baby_multi(2, 4) == 8
+    assert baby_multi(8, 2) == 16
+    assert baby_multi(2, 3) != 9
+    assert baby_multi(2, 30) != 24
+    assert baby_multi('str', 'str') is None
+    assert baby_multi(12, 0) == 0
+    assert baby_multi(0, 12) == 0
+    assert baby_multi(2, -2) == -4
+    assert baby_multi(-2, -2) == 4
